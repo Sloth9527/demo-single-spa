6db2e179076525782d0a703ebcc82bc819e10501
wangyun11 <wangyun11@58.com>
2021-12-10 15:19:05 +0800

    Mod server nav

--- example/single-spa-vue/server_vue/src/App.vue
+++ example/single-spa-vue/server_vue/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app" >
     <h1>server vue3</h1>
+    <div class="nav" >
+      <router-link to="/app1/home">app1 home</router-link>
+      <router-link to="/app1/about" >app1 about</router-link>
+      <router-link to="/app2/home" >app2 home</router-link>
+      <router-link to="/app2/about" >app2 about</router-link>
+    </div>
     <div id="microApp" >
 
     </div>
@@ -13,7 +19,7 @@ export default {
 }
 </script>
 
-<style>
+<style  >
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -21,5 +27,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.nav a{
+  display: inline-block;
+  padding: 0 10px;
+
+  text-decoration: none;  
+  font-size: 18px;
+  color: #000;
 }
 </style>
--- example/single-spa-vue/server_vue/src/main.js
+++ example/single-spa-vue/server_vue/src/main.js
@@ -2,6 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import apps from './apps';
 import { registerApplication, start } from 'single-spa';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [],
+});
 
 apps.forEach(app => {
     registerApplication(app);
@@ -13,4 +19,6 @@ createApp(Object.assign({}, App, {
             urlRerouteOnly: true
         });
     }
-})).mount('#app')
+}))
+.use(router)
+.mount('#app')
